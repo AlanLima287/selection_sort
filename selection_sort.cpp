20d8a529a726d4a4612d3073893cf96fd05c14f2
AlanLima287 <145388354+AlanLima287@users.noreply.github.com>
2024-05-07 16:25:41 -0300

    selection_sort complexity

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -15,3 +15,31 @@ void selection_sort(int* vector, int length) {
       swap(vector[lmin], vector[i]);
    }
 }
+
+// Para Luiz Felipe | Análise de Complexidade
+
+void selection_sort_ex(int* vector, int length) {
+
+   int i = 0;
+   while (i < length - 1) {
+      
+      int lmin = i;
+      
+      int j = i + 1;
+      while (j < length) {
+         if (vector[lmin] > vector[j]) 
+            lmin = j;
+
+         j++;
+      }
+
+      //swap(vector[lmin], vector[i]); 
+      {
+         int c = vector[lmin]; 
+         vector[lmin] = vector[i]; 
+         vector[i] = c;
+      }
+
+      i++;
+   }
+}
