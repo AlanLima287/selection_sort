5c29967f0684256bcc48d8caf489f2124fff13a0
AlanLima287 <alanlimanvc@gmail.com>
2024-05-07 16:40:58 -0300

    Pro Luiz

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -18,28 +18,107 @@ void selection_sort(int* vector, int length) {
 
 // Para Luiz Felipe | Análise de Complexidade
 
-void selection_sort_ex(int* vector, int length) {
+#include <iostream>
+#include <random>
+#include <time.h>
 
-   int i = 0;
-   while (i < length - 1) {
-      
-      int lmin = i;
-      
-      int j = i + 1;
-      while (j < length) {
-         if (vector[lmin] > vector[j]) 
-            lmin = j;
+enum RandomizationStyle {
+   RANDOM,
+   ORDERED,
+   INVERSED,
+   MOSTLY_ORDERED,
+   MOSTLY_INVERSED,
+};
 
-         j++;
-      }
+void populate(int* vector, int length, int seed, int style = RandomizationStyle::RANDOM) {
 
-      //swap(vector[lmin], vector[i]); 
-      {
-         int c = vector[lmin]; 
-         vector[lmin] = vector[i]; 
-         vector[i] = c;
-      }
+   srand(seed);
 
-      i++;
+   switch (style) {
+      case RandomizationStyle::RANDOM:
+
+         for (int i = 0; i < length; i++)
+            vector[i] = i + 1;
+
+         for (int i = 0; i < length; i++) {
+            int index = (int)(((float)rand() / (RAND_MAX + 1)) * length);
+            if (i != index) swap(vector[i], vector[index]);
+         }
+
+         break;
+
+      case RandomizationStyle::ORDERED:
+         for (int i = 0; i < length; i++)
+            vector[i] = i + 1;
+         break;
+
+      case RandomizationStyle::INVERSED:
+         for (int i = 0; i < length; i++)
+            vector[i] = length - i;
+         break;
+
+      case RandomizationStyle::MOSTLY_ORDERED:
+
+         for (int i = 0; i < length; i++)
+            vector[i] = i + 1;
+
+         for (int i = 0; i < length; i++) {
+            int index = i - 0x7 + (rand() & 0xf);
+
+            if (0 <= index && index < length && i != index)
+               swap(vector[i], vector[index]);
+         }
+
+         break;
+
+      case RandomizationStyle::MOSTLY_INVERSED:
+
+         for (int i = 0; i < length; i++)
+            vector[i] = length - i;
+
+         for (int i = 0; i < length; i++) {
+            int index = i - 0x7 + (rand() & 0xf);
+
+            if (0 <= index && index < length && i != index)
+               swap(vector[i], vector[index]);
+         }
+
+         break;
+
+      default: throw;
    }
+}
+
+int main(int argc, char** argv) {
+
+   int length;
+   std::cout << "Tamanho do vetor: "; std::cin >> length;
+
+   if (0 >= length || length > 8192) return 1;
+
+   int* vector = new int[length];
+   populate(vector, length, (int)time(0), RandomizationStyle::RANDOM);
+
+   // Selection sort aqui
+
+   for (int i = 0; i < length - 1; i++) {
+
+      int lmin = i;
+
+      for (int j = i + 1; j < length; j++) {
+         if (vector[lmin] > vector[j]) lmin = j;
+      }
+
+      // para visualização apenas
+      for (int i = 0; i < length; i++)
+         std::cout << vector[i] << ' ';
+      std::cout << '\n';
+
+      swap(vector[lmin], vector[i]);
+   }
+
+   // fim do selection sort
+
+   delete[] vector;
+   return 0;
 }
