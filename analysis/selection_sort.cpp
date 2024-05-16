cd856a3e5647659238771daca5c6c90bbe0561a8
AlanLima287 <alanlimanvc@gmail.com>
2024-05-16 17:39:10 -0300

    Commented version

--- analysis/selection_sort.cpp
+++ analysis/selection_sort.cpp
@@ -1,19 +1,28 @@
 void swap(int& a, int& b) {
-   int c = a; 
-   a = b; 
-   b = c;
+   int c = a; // c armazena a para que seu valor não seja perdido
+   a = b; // a recebe b
+   b = c; // b recebe o valor que pertencia a a
 }
 
 void selection_sort(int* vector, int length) {
 
+   // para cada posição, itera-se uma vez, exceto a última, 
+   // pois a ordenação de n - 1 elementos, implicará em ordenação total
    for (int i = 0; i < length - 1; i++) {
 
-      int lmin = i;
+      int lmin = i; // o mínimo local é inicialmente dado por i
 
+      // itera sobre a lista não ordenadada, de i + 1 até n
       for (int j = i + 1; j < length; j++) {
-         if (vector[lmin] > vector[j]) lmin = j;
+         
+         // se o elemento no mínimo local é maior que elemento em j, 
+         // então j será o novo mínimo local
+         if (vector[lmin] > vector[j]) 
+            lmin = j;
       }
 
+      // após a iteração, lmin será um mínimo local verdadeiro
+      // e este será posto no início da lista não ordenadada
       swap(vector[lmin], vector[i]);
    }
 }
